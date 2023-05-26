4484d220b74a3c5132a1914a96257b9e9fab7846
Xiaoling Tong <tonxiaol@sheridancollege.ca>
2023-05-26 12:20:39 -0400

    1st commit

---
+++ src/tempdemps23friam/TempDemps23FriAM.java
@@ -0,0 +1,17 @@
+
+package tempdemps23friam;
+
+/**
+ *
+ * @author tongx
+ */
+public class TempDemps23FriAM {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
